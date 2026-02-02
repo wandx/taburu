bbb50d7f3092ad559c2ef5cb03807eb4adc274f3
wandypurnomo <wandypurnomo92@gmail.com>
2026-02-02 11:34:24 +0700

    update provider

--- lib/src/taburu_view.dart
+++ lib/src/taburu_view.dart
@@ -13,48 +13,50 @@ class TaburuView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = Provider.of<TaburuController>(context, listen: false);
-    final deviceW = MediaQuery.sizeOf(context).width;
-    final gridH = deviceW / controller.gridSize;
-
     return ClipRRect(
       child: Container(
-        width: MediaQuery.sizeOf(context).width - (padding * 2),
-        padding: EdgeInsets.only(bottom: padding),
-        height: controller.gridCrossSize * gridH,
         child: Consumer<TaburuController>(
-          builder: (context, controller, child) => CanvasTouchDetector(
-            gesturesToOverride: const [
-              GestureType.onPanDown,
-              GestureType.onPanUpdate,
-              GestureType.onPanStart,
-              GestureType.onTapUp,
-              GestureType.onLongPressEnd,
-            ],
-            builder: (context) => CustomPaint(
-              painter: TaburuPainter(
-                context: context,
-                movable: true,
-                onTableTapped: (index) async {
-                  final table = controller.tables[index];
-                  final newTable = await showDialog<TaburuTable?>(
+          builder: (context, controller, child) {
+            final deviceW = MediaQuery.sizeOf(context).width;
+            final gridH = deviceW / controller.gridSize;
+            return Container(
+              width: MediaQuery.sizeOf(context).width - (padding * 2),
+              padding: EdgeInsets.only(bottom: padding),
+              height: controller.gridCrossSize * gridH,
+              child: CanvasTouchDetector(
+                gesturesToOverride: const [
+                  GestureType.onPanDown,
+                  GestureType.onPanUpdate,
+                  GestureType.onPanStart,
+                  GestureType.onTapUp,
+                  GestureType.onLongPressEnd,
+                ],
+                builder: (context) => CustomPaint(
+                  painter: TaburuPainter(
                     context: context,
-                    builder: (_) => TaburuAddDialog(
-                      table: table,
-                      width: controller.gridSize,
-                      height: controller.gridCrossSize,
-                    ),
-                  );
-                  if (newTable != null) {
-                    controller.updateTableOnIndex(index, newTable);
-                  }
-                },
-                onTableLongPress: (index) {
-                  controller.duplicateTable(index);
-                },
+                    movable: true,
+                    onTableTapped: (index) async {
+                      final table = controller.tables[index];
+                      final newTable = await showDialog<TaburuTable?>(
+                        context: context,
+                        builder: (_) => TaburuAddDialog(
+                          table: table,
+                          width: controller.gridSize,
+                          height: controller.gridCrossSize,
+                        ),
+                      );
+                      if (newTable != null) {
+                        controller.updateTableOnIndex(index, newTable);
+                      }
+                    },
+                    onTableLongPress: (index) {
+                      controller.duplicateTable(index);
+                    },
+                  ),
+                ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
